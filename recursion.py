24f7ad2680ae653f3dc0a380f85e1a7be3bc6d76
Vinmar Tipay <vinmartipay@Vinmars-MacBook-Pro-2.local>
2018-10-06 13:08:17 +0100

    Recursion algorithm

---
+++ recursion.py
@@ -0,0 +1,7 @@
+def factorial(base_case):
+    try:
+        if int(base_case) <= 1:
+            return base_case
+        return factorial(int(base_case)-1) * int(base_case)
+    except Exception as e:
+        return e.args
